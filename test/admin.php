7b104741d8820315d9ff217d7986e811b4c772c9
konkon-fox <kon-fox-kon@proton.me>
2025-12-29 23:06:18 +0900

    fix: 管理ページ内のファイルの読み書きとログインの安全性を向上。

--- test/admin.php
+++ test/admin.php
@@ -1,22 +1,18 @@
 <?php
 error_reporting(E_COMPILE_ERROR | E_RECOVERABLE_ERROR | E_ERROR | E_CORE_ERROR | E_PARSE);
 header('Content-Type: text/html; charset=UTF-8');
-if (!isset($_REQUEST['bbs'])) {
-    $_REQUEST['bbs'] = '';
-}
-if (!isset($_REQUEST['mode'])) {
-    $_REQUEST['mode'] = '';
-}
-if (!isset($_REQUEST['key'])) {
-    $_REQUEST['key'] = '';
-}
-$PATH = '../' . $_REQUEST['bbs'] . '/';
-$passfile = $PATH . 'passfile.cgi';
-$admin = @file_get_contents($passfile);
-if (!isset($_POST['password'])) {
-    $_POST['password'] = '';
-}
-if (!$_REQUEST['bbs'] || !$_POST['password']) {
+
+include './utils/safe-file-get-contents.php';
+
+// パラメータ取得
+$bbs = basename(trim($_POST['bbs'] ?? $_GET['bbs'] ?? ''));
+$safeBbs = htmlspecialchars($bbs, ENT_QUOTES, 'UTF-8');
+$mode = basename(trim($_GET['mode'] ?? ''));
+$safeMode = htmlspecialchars($mode, ENT_QUOTES, 'UTF-8');
+$threadKey = basename(trim($_GET['key'] ?? ''));
+$safeThreadKey = htmlspecialchars($threadKey, ENT_QUOTES, 'UTF-8');
+
+if (!$bbs || !$_POST['password']) {
     ?><!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -28,30 +24,43 @@ if (!$_REQUEST['bbs'] || !$_POST['password']) {
 <link rel="stylesheet" href="/static/a.css">
 </head>
 <body>
-<form class="form-signin" method="post" action="?bbs=<?=$_REQUEST['bbs'];?>&mode=<?=$_REQUEST['mode'];?>&key=<?=$_REQUEST['key'];?>">
+<form class="form-signin" method="post" action="?bbs=<?= $safeBbs; ?>&mode=<?= $safeMode; ?>&key=<?= $safeThreadKey; ?>">
 <h1 class="font-weight-normal mb-3">ログイン</h1>
-<input name="bbs" class="form-control" placeholder="ディレクトリ名" value="<?=$_REQUEST['bbs'];?>" required="">
-<input type="password" name="password" class="form-control" placeholder="パスワード" value="<?=$_POST['password'];?>" required="">
+<input name="bbs" class="form-control" placeholder="ディレクトリ名" value="<?= $safeBbs; ?>" required="">
+<input type="password" name="password" class="form-control" placeholder="パスワード" value="" required="">
 <p class="mb-3"></p>
 <input name="login_be_normal_user" class="btn btn-primary btn-block" type="submit" value="ログイン">
 </form>
 </body>
 </html>
 <?php
-      exit;
-} elseif (!password_verify($_POST['password'], $admin)) {
-    Finish('<b>パスワードが違います</b><div class="back"><a href="' . $_SERVER['HTTP_REFERER'] . '">← 戻る</a></div>');
+    exit;
+}
+
+// パスワードハッシュ取得
+$PATH = '../' . $bbs . '/';
+$passfile = $PATH . 'passfile.cgi';
+$admin = safe_file_get_contents($passfile);
+if ($admin === false) {
+    Finish('<b>パスワードファイルの取得に失敗しました。</b>');
+}
+if (!isset($_POST['password'])) {
+    $_POST['password'] = '';
+}
+// パスワード照合
+if (!password_verify($_POST['password'], $admin)) {
+    Finish('<b>パスワードが違います。</b>');
 }
 
 // 設定ファイルを読む
 $setfile = $PATH . 'setting.json';
 $settxt = $PATH . 'SETTING.TXT';
 $SET = '';
-if (is_file($setfile)) {
-    $SETTING = json_decode(file_get_contents($setfile), true);
-} else {
-    Finish('<b>設定ファイルがありません</b><div class="back"><a href="' . $_SERVER['HTTP_REFERER'] . '">← 戻る</a></div>');
+$setContent = safe_file_get_contents($setfile);
+if ($setContent === false) {
+    Finish('<b>設定ファイルがありません</b>');
 }
+$SETTING = json_decode($setContent, true);
 
 // 設定の一覧ページ
 require './operate/settinglist.php';
