600f0d5b0cce96f08a9e00d626ddac990cda1851
konkon-fox <kon-fox-kon@proton.me>
2025-12-29 22:02:53 +0900

    fix: コマンドのinclude順を変更。

--- test/bbs-main.php
+++ test/bbs-main.php
@@ -568,12 +568,12 @@ include './extend/extra-commands/utilities/add-system-message.php';
 @include './extend/extra-commands/set-774.php';
 // !gobi設定
 @include './extend/extra-commands/set-gobi.php';
+// !ngk適用
+@include './extend/extra-commands/apply-ngk.php';
 // !774適用
 @include './extend/extra-commands/apply-774.php';
 // !rmjコマンド
 @include './extend/extra-commands/rmj.php';
-// !ngk適用
-@include './extend/extra-commands/apply-ngk.php';
 // !gobi適用
 @include './extend/extra-commands/apply-gobi.php';
 // スレ状態更新処理
